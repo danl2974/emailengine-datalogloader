a5692f185216e6211e811c3905b612614c4a6957
dlubin <dlubin@ABO007.dev.local>
2014-02-21 16:58:57 -0500

    Feb 21 4:58PM

---
+++ src/com/lambdus/emailengine/dataloader/EmailClick.java
@@ -0,0 +1,18 @@
+package com.lambdus.emailengine.dataloader;
+
+import java.sql.Timestamp;
+
+public class EmailClick {
+
+	public Timestamp timestamp;
+	
+	public String uuid;
+	
+	public String email;
+	
+	public int templateid;
+	
+	public String link;
+	
+	
+}
---
+++ src/com/lambdus/emailengine/dataloader/EmailOpen.java
@@ -0,0 +1,15 @@
+package com.lambdus.emailengine.dataloader;
+
+import java.sql.Timestamp;
+
+public class EmailOpen {
+	
+	public Timestamp timestamp;
+	
+	public String uuid;
+	
+	public String email;
+	
+	public int templateid;
+
+}
